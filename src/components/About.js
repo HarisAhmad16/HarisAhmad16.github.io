fcd1b5a14eb93a21314105a35aed5c1ec2e1eb6f
HarisAhmad16 <harisahmad04@hotmail.com>
2024-01-03 19:22:45 -0800

    Experience details and sizing

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import '../styles/about.css';
 
 class About extends React.Component {
@@ -13,11 +14,14 @@ class About extends React.Component {
           <h3 className="details">
             Hello! I'm a dedicated fourth-year Computer Science student at Simon Fraser University actively seeking my third software engineering internship. My academic journey has been enriched by a variety of courses, and the experiences within the teams I've worked with have been instrumental in shaping my current skill set. My passion for technology extends beyond the classroom, as I am keen on staying up to date with the latest developments in the field. I am actively seeking opportunities to contribute my skills and enthusiasm to a company that will allow me to grow and learn as a Software Engineer.
           </h3>
-          
+
           <h3 className="lowdetails">
-          Feel free to contact or connect with me to discuss opportunities, sharing insights or explore new collaborations together!
+            Feel free to contact or connect with me to discuss opportunities, sharing insights or explore new collaborations together!
           </h3>
           <div className="social-icons">
+            <a href="mailto:mha149@sfu.ca" className="mail-icon">
+              <FontAwesomeIcon icon={faEnvelope} size="4x" />
+            </a>
             <a href="https://www.linkedin.com/in/harisahmad16/" target="_blank" rel="noopener noreferrer" className="linkedin-icon">
               <FontAwesomeIcon icon={faLinkedin} size="4x" />
             </a>

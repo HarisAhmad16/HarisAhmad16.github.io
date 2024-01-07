5d58ac2304bd9e14f460cee68df6e62d58337d77
HarisAhmad16 <harisahmad04@hotmail.com>
2024-01-06 19:41:48 -0800

    About details updated along with design

--- src/components/About.js
+++ src/components/About.js
@@ -8,17 +8,21 @@ class About extends React.Component {
     return (
       <div id="about" className="about">
         <h1 className="title">About Me</h1>
-        <a href="https://www.linkedin.com/in/harisahmad16/" target="_blank" rel="noopener noreferrer" className="linkedin-icon">
-          <FontAwesomeIcon icon={faLinkedin} size="4x" />
-        </a>
-        <a href="https://github.com/HarisAhmad16" target="_blank" rel="noopener noreferrer" className="github-icon">
-          <FontAwesomeIcon icon={faGithub} size="4x" />
-        </a>
+        <div className="intro-text">
+          <h2 className="about-details">Hi, I'm a 4th year Computer Science student at Simon Fraser University. I'm passionate about everything software related and love to to create meaningful projects. Seeking for my 3rd internship, I would love to bring my talent to a company that strives to create a positive impact and values their work!</h2>
+          <h2 className="about-connect">Feel free to connect with me or reach out to discuss opportunities, sharing insights or explore new collaborations together!</h2>
+          <div className="social-icons">
+            <a href="https://www.linkedin.com/in/harisahmad16/" target="_blank" rel="noopener noreferrer" className="linkedin-icon">
+              <FontAwesomeIcon icon={faLinkedin} size="9x" style={{ color: '#0077B5' }} />
+            </a>
+            <a href="https://github.com/HarisAhmad16" target="_blank" rel="noopener noreferrer" className="github-icon">
+              <FontAwesomeIcon icon={faGithub} size="9x" style={{ color: '#ffffff' }} />
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-// Hello! I'm a dedicated fourth-year Computer Science student at Simon Fraser University actively seeking my third software engineering internship. My academic journey has been enriched by a variety of courses, and the experiences within the teams I've worked with have been instrumental in shaping my current skill set. My passion for technology extends beyond the classroom, as I am keen on staying up to date with the latest developments in the field. I am actively seeking opportunities to contribute my skills and enthusiasm to a company that will allow me to grow and learn as a Software Engineer.
-// Feel free to connect with me or reach out to discuss opportunities, sharing insights or explore new collaborations together!
 export default About;

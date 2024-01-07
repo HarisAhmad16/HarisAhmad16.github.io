64dd8a63ed585fd729a57c6212e01c4e4bf3ff3b
HarisAhmad16 <harisahmad04@hotmail.com>
2024-01-07 14:27:49 -0800

    V1

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,6 +3,7 @@ import '../styles/experience.css';
 import Section from "./Section";
 import ccs from '../images/ccs.jpg'
 import article from '../images/article.jpg'
+import bgc from '../images/bgc.jpeg'
 
 class Experience extends React.Component {
   render() {
@@ -13,14 +14,36 @@ class Experience extends React.Component {
           <Section
             heading="COAST CAPITAL SAVINGS"
             subheading="Software Developer Intern"
-            details="As a member of the Core Banking Team, I had the opportunity to explore and deepen my understanding of the software responsible for managing our banking transactions and accounts. I actively contributed by performing various data fixes that addressed numerous financial impacts across different database records. In this role, I created Python scripts to export and evaluate monetary values in CSV format, enabling me to generate solutions and gather key insights for the business. I developed multi-threaded programs designed to check and resolve more than 100,000 incorrect credit card statuses. Also, I had the opportunity to expand my knowledge of APIs through one-on-one sessions which allowed me to apply this knowledge in correcting interest payment errors in the core banking system by configuring the penalty rate API, resolving incorrect charges for 1200 bank members."
+            details={[
+              " Developed a multi-threaded program to analyze over 100,000 credit card statuses, resulting in a condition correction for all stolen, damaged, inactive, or lost credit cards",
+              "Created scripts in Python that evaluated and exported banking data into CSV files to help underly the businesses current needs and a variety of differences of monetary value",
+              "Performed multiple data fixes by re-evaluating records from the Oracle database and JAR files, resolving a total financial impact of up to $500,000",
+              "Corrected the interest payment in the core banking system by configuring the penalty rate API, resolving incorrect charges to 1200 bank members"
+            ]}
             imageUrl={ccs}
           />
           <Section
             heading="ARTICLE"
             subheading="Full Stack Software Engineer Intern"
-            details="As a member of the Warehouse Team, I participated in all areas of the software development lifecycle, including building solutions for web storefronts and internal supply chain applications, as well as creating test automation suites. The main project I worked on involved deploying a tool on an RF Scanner to address box-scanning issues for warehouse workers. I utilized various technologies such as Vue, TypeScript, Java, Spring Boot, Apache Kafka, and Docker. The tool's development was guided by Figma prototypes from the UI/UX team and tested using Jest and JUnit. I also had the opportunity to delve deeper into the business architectures and gain a more thorough understanding of the migration process from a Monolithic to Microservices architecture."
+            details={[
+              "Utilized Topics and Brokers from Apache Kafka to handle concurrent scan requests and real time processing of box counts, resulting in a 40% increase in time of obtaining warehouse data",
+              "Designed the user interface of a warehouse tool connected to RF Scanners using Vue & TypeScript, resulting in an enhanced user experience and enabling easy usability",
+              "Developed endpoints in Spring Boot to seamlessly redirect and validate box information, thereby leading to reduced errors and improved data accuracy of 1000+ scans",
+              "Created tests in JUnit and Jest to replicate the interactions and designs specified in the Figma prototype, achieving an overall test coverage of 80%",
+              "Contributed to meetings on migrating the Monolithic architecture to Microservices, addressing necessary changes and database connections to align with the company's growth"
+            ]}
             imageUrl={article}
+          />
+          <h2 className="exptitle">Projects</h2>
+          <Section
+            heading="BGC Engineering Inventory System"
+            subheading="An app to track/scan BGC's inventory"
+            details={[
+              "Leveraged barcode data to create an inventory system for the company, BGC Engineering, reducing the time spent handling each stock through scanning and CRUD functionality",
+              "Integrated the Google Maps API displaying a visual graphic of the location of an item resulting in the management being able to track and locate it",
+              "Secured the system through user authorization and authentication with Passport.js and BCrypt hashing allowing members to correctly sign in and create accounts based off of current company position"
+            ]}
+            imageUrl={bgc}
           />
         </div>
       </div>
--- src/components/Section.js
+++ src/components/Section.js
@@ -16,9 +16,11 @@ const Section = ({ heading, details, imageUrl, subheading }) => {
           <h3 className='subheading'>
             {subheading}
           </h3>
-          <p>
-            {details}
-          </p>
+          <ul>
+            {details.map((detail, index) => (
+              <li key={index}>{detail}</li>
+            ))}
+          </ul>
         </div>
       </div>
     </main>
@@ -28,7 +30,7 @@ const Section = ({ heading, details, imageUrl, subheading }) => {
 Section.propTypes = {
   heading: PropTypes.string.isRequired,
   subheading: PropTypes.string.isRequired,
-  details: PropTypes.string.isRequired,
+  details: PropTypes.arrayOf(PropTypes.string).isRequired,
   imageUrl: PropTypes.string.isRequired,
 };
 
